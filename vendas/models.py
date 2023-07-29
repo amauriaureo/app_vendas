b939d759418d43c6ebfe414fc5baaa7a7ab62651
Amauri Rodrigues <amauriaureo@gmail.com>
2023-07-29 10:18:58 -0300

    pedido

--- vendas/models.py
+++ vendas/models.py
@@ -6,3 +6,10 @@ class Cliente(models.Model):
     nome = models.CharField(max_length=100)
     email = models.EmailField()
     cpf = models.CharField(max_length=11, unique=True)
+
+
+class Pedido(models.Model):
+
+    cliente = models.ForeignKey(Cliente, models.CASCADE)
+    data = models.DateField()
+    observacao = models.TextField(blank=True)
