51c2b44f649817097107b574518c003b29b03b23
Amauri Rodrigues <amauriaureo@gmail.com>
2023-07-29 10:22:22 -0300

    itempedido

--- vendas/models.py
+++ vendas/models.py
@@ -13,3 +13,11 @@ class Pedido(models.Model):
     cliente = models.ForeignKey(Cliente, models.CASCADE)
     data = models.DateField()
     observacao = models.TextField(blank=True)
+
+
+class ItemPedido(models.Model):
+
+    pedido = models.ForeignKey(Pedido, models.CASCADE)
+    produto = models.CharField(max_length=100)
+    quantidade = models.IntegerField(default=1)
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
