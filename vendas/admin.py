4a8b161806c1b584a7041264c3b4a802b9da7e77
Amauri Rodrigues <amauriaureo@gmail.com>
2023-07-29 11:31:41 -0300

    tabela de vendas no admin

--- vendas/admin.py
+++ vendas/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from vendas.models import Cliente, Pedido, ItemPedido
+
+
+class ClienteAdmin(admin.ModelAdmin):
+
+    list_display = ['nome', 'email', 'cpf']
+    search_fields = ['nome', 'email', 'cpf']
+
+
+class PedidoAdmin(admin.ModelAdmin):
+
+    list_display = ['cliente', 'data']
+
+
+class ItemAdmin(admin.ModelAdmin):
+
+    list_display = ['pedido', 'produto', 'quantidade', 'preco']
+
+
+admin.site.register(Cliente, ClienteAdmin)
+admin.site.register(Pedido, PedidoAdmin)
+admin.site.register(ItemPedido, ItemAdmin)
